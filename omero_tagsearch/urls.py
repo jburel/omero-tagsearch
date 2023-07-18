525bb3cab0846fe429b7de08b3f28ac5689231df
Jean-Marie Burel <j.burel@dundee.ac.uk>
2023-07-18 21:48:34 +0100

    urls.py uses re_path

--- omero_tagsearch/urls.py
+++ omero_tagsearch/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
     # index 'home page' of the webtagging app
-    url(r"^$", views.index, name="tagsearch"),
+    re_path(r"^$", views.index, name="tagsearch"),
     # index 'home page' of the webtagging app
-    url(r"^images$", views.tag_image_search, name="wtsimages"),
+    re_path(r"^images$", views.tag_image_search, name="wtsimages"),
 ]
